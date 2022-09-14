9ec503536b6fbf4a7421a98cf3102414cbf73ed4
syahendraazmii <102424767+syahendraazmii@users.noreply.github.com>
2022-09-14 19:25:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Login
+{
+    public class inputClient
+    {
+        public string UName { get; set; }
+        public string Pass { get; set; }
+
+        public int LengthUser, LengthPassword;
+    }
+    class Client
+    {
+        static void Main()
+        {
+            inputClient client = new inputClient();
+            Console.WriteLine("Your Username : ");
+            client.UName = Console.ReadLine();
+
+            Console.WriteLine("Your Password : ");
+            client.Pass = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine(client.UName.Length + "_" + client.UName + " " + client.Pass.Length + "_" + client.Pass);
+            string jsonString = JsonSerializer.Serialize(client);
+
+            Console.WriteLine(jsonString);
+            Console.ReadLine();
+        }
+    }
+}
